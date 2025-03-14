df3e239a59d87188045be37e04db06570043425d
Alex H <alex.hatfield.tech@gmail.com>
2025-03-14 17:54:20 +0000

    fix: add missing character list view and update serializer

--- restful_api/views.py
+++ restful_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Character
+from .serializers import CharacterSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def character_list(request):
+    characters = Character.objects.all()
+    serializer = CharacterSerializer(characters, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
