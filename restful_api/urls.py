548e6fbb8c7d952b39d232d7f1133f38bb6b9806
Alex Hatfield <134648143+HatfieldAlex@users.noreply.github.com>
2025-03-26 17:58:09 +0000

    feat(api): expose CharacterCreate view at /characters/create/

--- restful_api/urls.py
+++ restful_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import character_list
+from .views import character_list, CharacterCreate
 
 urlpatterns = [
     path('characters/', character_list, name='character-list'),
+    path('characters/create/', CharacterCreate.as_view(), name='character-create'),
 ]
