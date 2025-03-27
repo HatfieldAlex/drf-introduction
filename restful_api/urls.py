a1f3c263a2047be242e5af2d7f450495d224176d
Alex Hatfield <134648143+HatfieldAlex@users.noreply.github.com>
2025-03-27 19:07:05 +0000

    refactor: update character list URL to 'characters/list/' for consistency

--- restful_api/urls.py
+++ restful_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import character_list, CharacterCreate
 
 urlpatterns = [
-    path('characters/', character_list, name='character-list'),
+    path('characters/list/', character_list, name='character-list'),
     path('characters/create/', CharacterCreate.as_view(), name='character-create'),
 ]
