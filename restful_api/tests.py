efafdbbc8b8422604ffb1ac8ca43ef55c4996f5f
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-04-08 17:12:09 +0100

    test: outline test classes for unit and integration testing

--- restful_api/tests.py
+++ restful_api/tests.py
@@ -1,1 +1,8 @@
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APIClient
+
+#Unit Testing
+#In DRF, APIRequestFactory is one of the main tools for testing views in isolation
+
+#Intrgration Testing
+#In DRF, APIClient is a primary tool for testing the full request/response cycle
+
