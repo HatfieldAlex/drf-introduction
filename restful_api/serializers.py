82fe9425711c9c186442116f194c66355cbdf2db
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-04-10 17:34:13 +0100

    docs: add comment on serializer role in DRF using docstring

--- restful_api/serializers.py
+++ restful_api/serializers.py
@@ -5,3 +5,7 @@ class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ['id', 'name']
+
+"""
+The serializer is critical to DRF. Unlike regular Django where data is exposed to HTML through templates, DRF needs to convert data into JSON format. Serializers handle this transformation.
+"""
